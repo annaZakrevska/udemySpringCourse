25e654d0bfee8ad6c8cce9b60f72ef865c9d52c6
Anna Zakrevska <anna.zakrevska@sollers.eu>
2022-11-16 19:46:11 +0100

    Adding genarated todoapp

---
+++ todoapp/src/test/java/github/udemySpringCourse/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package github.udemySpringCourse.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
